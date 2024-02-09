c7625d811f445f30298becec9af25902636db9c2
student <bfhf2222@gmail.com>
2024-02-09 16:24:57 +0400

    first

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "Миссия Колонизация Марса"
-
-
-@app.route('/')
-@app.route('/index')
-def index2():
-    return "И на Марсе будут яблони цвести!"
-
-
-@app.route('/')
-@app.route('/promotion')
-def index3():
-    return """Человечество вырастает из детства.</br>
-Человечеству мала одна планета.</br>
-Мы сделаем обитаемыми безжизненные пока планеты.</br>
-И начнем с Марса!</br>
-Присоединяйся!"""
-
-
-@app.route('/')
-@app.route('/image_mars')
-def index4():
-    with open('index.html', 'r', encoding='utf-8') as f:
-        return f.read()
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title=title)
 
 
 if __name__ == '__main__':
