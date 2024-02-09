dbd1c1845c8c7d0d443256b966468819e27e3458
student <bfhf2222@gmail.com>
2024-02-09 17:31:16 +0400

    second

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/traning/<prof>')
+def index1(prof):
+    return render_template('traning.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
